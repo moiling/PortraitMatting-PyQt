444319995d334e47f21704262bd6b9fc8c7abb60
moi <moiling1996@gmail.com>
2022-02-09 22:44:23 +0800

    use relative path instead of absolute path.

--- resource/style.py
+++ resource/style.py
@@ -2,13 +2,13 @@ import os
 
 
 def read_qss(style):
-    style = '{}/resource/qss/{}.qss'.format(os.path.abspath(os.curdir), style)
+    style = '{}/resource/qss/{}.qss'.format(os.path.relpath(os.curdir), style)
     with open(style, 'r') as f:
         return f.read()
 
 
 def icon_str(icon):
-    return '{}/resource/icon/{}.png'.format(os.path.abspath(os.curdir), icon)
+    return '{}/resource/icon/{}.png'.format(os.path.relpath(os.curdir), icon)
 
 
 scroll_bar = read_qss('scroll_bar')
